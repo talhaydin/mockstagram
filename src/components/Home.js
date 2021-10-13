b3754481126c152da6c4c1c96d2f3a6653a620d9
Talha_Aydin <mt.aydin@outlook.com>
2021-10-13 22:33:34 +0300

    homepage almost complete

---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './Home.scss';
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyapi.io/data/v1/post?limit=10', {
+      headers: {
+        'app-id': '6166fd506281a5754336756e',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setPosts(data.data));
+  }, []);
+
+  return (
+    <div className="postContainer">
+      {posts.map((item) => (
+        <div key={item.id} className="postCards">
+          <div className="personTag">
+            <img src={item.owner.picture} alt="post owner" />
+            <h4>
+              {item.owner.firstName} {item.owner.lastName}
+            </h4>
+          </div>
+          <img src={item.image} alt="post img" className="postImage" />
+          <h4>{item.text}</h4>
+          <h4>❤️{item.likes} Likes</h4>
+          <p>{item.publishDate}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
